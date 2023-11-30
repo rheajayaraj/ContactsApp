85d65ac5560d9a63241559a8e149ebcba7582d38
rheajayaraj <121881794+rheajayaraj@users.noreply.github.com>
2023-11-30 09:38:13 +0530

    Unique tags display

--- controllers/contacts/viewtagscontroller.js
+++ controllers/contacts/viewtagscontroller.js
@@ -6,28 +6,24 @@ const verify = require('../../middleware/jwtverify');
 module.exports = async (req, res) => {
   try {
     decryptedData = await decrypt(req.body.JWT_token);
-
     const decoded = await verify(decryptedData);
-    console.log(decoded);
-
     const user = await User.findById(decoded.id);
     if (user) {
-      const cons = await Contacts.find({ user_id: user.id });
-      let data = [];
-
-      cons.forEach((contact) => {
+      const contacts = await Contacts.find({ user_id: user.id });
+      let tagsSet = new Set();
+      contacts.forEach((contact) => {
         contact.tags.forEach((tag) => {
-          if (tag === req.body.tag) {
-            data.push(contact.name);
-          }
+          tagsSet.add(tag);
         });
       });
-
-      if (data.length > 0) {
-        res.json(data);
+      const uniqueTags = [...tagsSet];
+      if (uniqueTags.length > 0) {
+        res.json(uniqueTags);
       } else {
         res.json({ message: 'No contacts found with the specified tag' });
       }
+    } else {
+      res.status(404).json({ message: 'User not found' });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
